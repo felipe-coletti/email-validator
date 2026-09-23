e0a25feb8514f1077894a868254024cf87eef7f8
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2026-09-23 13:48:37 -0300

    feat: add initial files

---
+++ js/main.js
@@ -0,0 +1,34 @@
+import { isValid } from './validate.js'
+
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const result = document.getElementById('result')
+
+const setValidationState = (state, message) => {
+    input.classList.remove('success', 'error')
+    result.classList.remove('success', 'error')
+
+    input.classList.add(state)
+    result.classList.add(state)
+
+    result.textContent = message
+    result.hidden = false
+}
+
+input.addEventListener('focus', () => {
+    result.hidden = true
+})
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const email = input.value.trim()
+
+    if (!email) {
+        setValidationState('error', 'Digite um endereço de e-mail')
+    } else if (isValid(email)) {
+        setValidationState('success', 'E-mail válido')
+    } else {
+        setValidationState('error', 'Digite um endereço de e-mail válido')
+    }
+})
